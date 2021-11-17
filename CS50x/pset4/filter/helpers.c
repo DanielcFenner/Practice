9ee68d49c810f7bf60907120eee74db5cf13b7ac
Danielcfenner <dcfenner@gmai..com>
2021-11-16 23:01:19 -0500

    sry dane

--- CS50x/pset4/filter/helpers.c
+++ CS50x/pset4/filter/helpers.c
@@ -4,10 +4,10 @@
 // Convert image to grayscale
 void grayscale(int height, int width, RGBTRIPLE image[height][width])
 {
-    int avg;
-    for (int i = 0; i < height - 1; i++)
+    BYTE avg;
+    for (int i = 0; i < height; i++)
     {
-        for (int k = 0; k < width - 1; k++)
+        for (int k = 0; k < width; k++)
         {
             avg = round((float)(image[i][k].rgbtBlue + image[i][k].rgbtRed + image[i][k].rgbtGreen) / 3);
             image[i][k].rgbtBlue = avg, image[i][k].rgbtGreen = avg, image[i][k].rgbtRed = avg;
@@ -19,22 +19,20 @@ void grayscale(int height, int width, RGBTRIPLE image[height][width])
 // Convert image to sepia
 void sepia(int height, int width, RGBTRIPLE image[height][width])
 {
-    BYTE sepiaRed, sepiaGreen, sepiaBlue;
-    float originalRed, originalGreen, originalBlue;
-    for (int i = 0; i < height - 1; i++)
+    float sepiaRed, sepiaGreen, sepiaBlue;
+    for (int i = 0; i < height; i++)
     {
-        for (int j = 0; j < height - 1; j++)
+        for (int j = 0; j < width; j++)
         {
-            originalRed = image[i][j].rgbtRed;
-            originalGreen = image[i][j].rgbtGreen;
-            originalBlue = image[i][j].rgbtBlue;
-            sepiaRed = round((.393 * originalRed) + (.349 * originalGreen) + (.189 * originalBlue));
+            sepiaRed = (.393 * image[i][j].rgbtRed) + (.769 * image[i][j].rgbtGreen) + (.189 * image[i][j].rgbtBlue);
             if (sepiaRed > 255) sepiaRed = 255;
-            sepiaGreen = round((.349 * originalRed) + (.686 * originalGreen) + (.168 * originalBlue));
+            sepiaGreen = (.349 * image[i][j].rgbtRed) + (.686 * image[i][j].rgbtGreen) + (.168 * image[i][j].rgbtBlue);
             if (sepiaGreen > 255) sepiaGreen = 255;
-            sepiaBlue = round((.272 * originalRed) + (.534 * originalGreen) + (.131 * originalBlue));
+            sepiaBlue = (.272 * image[i][j].rgbtRed) + (.534 * image[i][j].rgbtGreen) + (.131 * image[i][j].rgbtBlue);
             if (sepiaBlue > 255) sepiaBlue = 255;
-            image[i][j].rgbtRed = sepiaRed, image[i][j].rgbtGreen = sepiaGreen, image[i][j].rgbtBlue = sepiaBlue;
+            image[i][j].rgbtBlue = round(sepiaBlue);
+            image[i][j].rgbtGreen = round(sepiaGreen);
+            image[i][j].rgbtRed = round(sepiaRed);
         }        
     }  
     return;
@@ -44,18 +42,104 @@ void sepia(int height, int width, RGBTRIPLE image[height][width])
 void reflect(int height, int width, RGBTRIPLE image[height][width])
 {
     RGBTRIPLE imageCopy[height][width];
-    for (int i = 0; i < height - 1; i++)
+    for (int i = 0; i < height; i++)
     {
-        for (int j = 0; j < width - 1; j++)
+        for (int j = 0; j < width; j++)
         {
-            image[i][j] = imageCopy[i][width - 1 - j];
+            imageCopy[i][j] = image[i][j];
         }       
     }
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            image[i][j] = imageCopy[i][width - j];
+        }       
+    }   
     return;
 }
 
 // Blur image
 void blur(int height, int width, RGBTRIPLE image[height][width])
-{
+{   
+    int avgRed;
+    int avgBlue;
+    int avgGreen;
+    int avgCounter;
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            if (i - 1 >= 0 && j - 1 >= 0)
+            {
+                avgRed += image[i - 1][j - 1].rgbtRed;
+                avgBlue += image[i - 1][j - 1].rgbtBlue;
+                avgGreen += image[i - 1][j - 1].rgbtGreen;
+                avgCounter++;
+            }
+            if (i - 1 >= 0 && j >= 0)
+            {
+                avgRed += image[i - 1][j].rgbtRed;
+                avgBlue += image[i - 1][j].rgbtBlue;
+                avgGreen += image[i - 1][j].rgbtGreen;
+                avgCounter++;
+            }
+            if (i - 1 >= 0 && j + 1 <= width)
+            {
+                avgRed += image[i - 1][j + 1].rgbtRed;
+                avgBlue += image[i - 1][j + 1].rgbtBlue;
+                avgGreen += image[i - 1][j + 1].rgbtGreen;
+                avgCounter++;
+            }
+            if (i >= 0 && j - 1 >= 0)
+            {
+                avgRed += image[i][j - 1].rgbtRed;
+                avgBlue += image[i][j - 1].rgbtBlue;
+                avgGreen += image[i][j - 1].rgbtGreen;
+                avgCounter++;
+            }
+            if (i >= 0 && j >= 0)
+            {
+                avgRed += image[i][j].rgbtRed;
+                avgBlue += image[i][j].rgbtBlue;
+                avgGreen += image[i][j].rgbtGreen;
+                avgCounter++;
+            }
+            if (i >= 0 && j + 1 <= width)
+            {
+                avgRed += image[i][j + 1].rgbtRed;
+                avgBlue += image[i][j + 1].rgbtBlue;
+                avgGreen += image[i][j + 1].rgbtGreen;
+                avgCounter++;
+            }
+            if (i + 1 <= height && j - 1 >= 0)
+            {
+                avgRed += image[i + 1][j - 1].rgbtRed;
+                avgBlue += image[i + 1][j - 1].rgbtBlue;
+                avgGreen += image[i + 1][j - 1].rgbtGreen;
+                avgCounter++;
+            }
+            if (i + 1 <= height && j >= 0)
+            {
+                avgRed += image[i + 1][j].rgbtRed;
+                avgBlue += image[i + 1][j].rgbtBlue;
+                avgGreen += image[i + 1][j].rgbtGreen;
+                avgCounter++;
+            }
+            if (i + 1 <= height && j + 1 <= width)
+            {
+                avgRed += image[i + 1][j + 1].rgbtRed;
+                avgBlue += image[i + 1][j + 1].rgbtBlue;
+                avgGreen += image[i + 1][j + 1].rgbtGreen;
+                avgCounter++;
+            }
+            image[i][j].rgbtRed = round((float)avgRed / avgCounter);
+            image[i][j].rgbtBlue = round((float)avgBlue / avgCounter);
+            image[i][j].rgbtGreen = round((float)avgGreen / avgCounter);
+            avgRed = 0, avgBlue = 0, avgGreen = 0, avgCounter = 0;            
+        }
+        
+    }
+    
     return;
 }
