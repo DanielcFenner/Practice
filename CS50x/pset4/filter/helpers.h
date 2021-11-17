e5d450bbcb8d91e8c2135851e42fdf9efe9fa31a
Danielcfenner <dcfenner@gmai..com>
2021-11-16 20:32:55 -0500

    kjh

--- CS50x/pset4/filter/helpers.h
+++ CS50x/pset4/filter/helpers.h
@@ -3,17 +3,16 @@
 // Convert image to grayscale
 void grayscale(int height, int width, RGBTRIPLE image[height][width])
 {
+    int avg;
     for (int i = 0; i < height - 1; i++)
     {
         for (int k = 0; k < width - 1; k++)
         {
-            image[i][k].rgbtBlue = (image[i][k].rgbtBlue + image[i][k].rgbtRed + image[i][k].rgbtGreen) / 3;
-            image[i][k].rgbtGreen = (image[i][k].rgbtBlue + image[i][k].rgbtRed + image[i][k].rgbtGreen) / 3;
-            image[i][k].rgbtRed = (image[i][k].rgbtBlue + image[i][k].rgbtRed + image[i][k].rgbtGreen) / 3;
+            avg = round((float)(image[i][k].rgbtBlue + image[i][k].rgbtRed + image[i][k].rgbtGreen) / 3);
+            image[i][k].rgbtBlue = avg, image[i][k].rgbtGreen = avg, image[i][k].rgbtRed = avg;
         }
         
     }
-    
 }
 
 // Convert image to sepia
