f20b9c59add6493aa31c57496efcc78869db0160
Danielcfenner <dcfenner@gmai..com>
2021-11-16 20:18:22 -0500

    h

--- CS50x/pset4/filter/helpers.h
+++ CS50x/pset4/filter/helpers.h
@@ -3,15 +3,13 @@
 // Convert image to grayscale
 void grayscale(int height, int width, RGBTRIPLE image[height][width])
 {
-    int average;
     for (int i = 0; i < height - 1; i++)
     {
         for (int k = 0; k < width - 1; k++)
         {
-            average = (image[i][k].rgbtBlue + image[i][k].rgbtRed + image[i][k].rgbtGreen) / 3;
-            image[i][k].rgbtBlue = average;
-            image[i][k].rgbtGreen = average;
-            image[i][k].rgbtRed = average;
+            image[i][k].rgbtBlue = (image[i][k].rgbtBlue + image[i][k].rgbtRed + image[i][k].rgbtGreen) / 3;
+            image[i][k].rgbtGreen = (image[i][k].rgbtBlue + image[i][k].rgbtRed + image[i][k].rgbtGreen) / 3;
+            image[i][k].rgbtRed = (image[i][k].rgbtBlue + image[i][k].rgbtRed + image[i][k].rgbtGreen) / 3;
         }
         
     }
