9230979f991cb99072526462675afb626d2d6af8
Daniel Fenner <85763192+DanielcFenner@users.noreply.github.com>
2021-11-19 08:41:56 -0500

    CS50x IDE is down, can't test code

--- CS50x/pset4/recovery/recover.c
+++ CS50x/pset4/recovery/recover.c
@@ -1,11 +1,35 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdint.h>
 
 int main(int argc, char *argv[])
 {
     if (argc != 2)
+    {
         printf("Usage: ./recover filename\n");
+        return 0;
+    }
 
-    char *infile = argv[1];
-    printf("%s", infile);
+    FILE *infile = fopen(argv[1], "r");
+    FILE *outfile;
+    uint8_t buffer[512];
+    int jpgCounter = 0;
+    char *filename = malloc(8 * sizeof(char));
+    while (fread(&buffer, sizeof(buffer), 512, infile))
+    {
+        if (buffer[0] == 0xff && buffer[1] == 0xd8 && buffer[2] == 0xff && (buffer[3] & 0xf0) == 0xe0)
+        {
+            sprintf(filename, "%03i.jpg", jpgCounter);
+            outfile = fopen(filename, "w");
+            fwrite(&buffer, sizeof(buffer), 512, outfile);
+            jpgCounter++;
+        }
+        else
+        {
+            fwrite(&buffer, sizeof(buffer), 512, outfile);
+        }
+    }
+    free(filename);
+    fclose(infile);
+    fclose(outfile);
 }
